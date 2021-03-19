d131e76f40caf21d0a7c8122265821725ad35c1b
Andrew Shaw <andrewjshaw98@gmail.com>
2021-03-19 08:12:38 +0000

    idle, run and die animations. 'n' controls start, 'm' controls lives

---
+++ Assets/Scripts/animationController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animationController : MonoBehaviour
+{
+    Animator animator;
+    int lives;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        lives = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("n"))
+        {
+            animator.SetBool("startGame", true);
+            
+        }
+        else if(Input.GetKeyDown("m"))
+        {
+            lives -= 1;
+            if (lives == 0)
+            {
+                animator.SetBool("dead", true);
+            }
+            Debug.Log("lives: " + lives);
+        }
+    }
+}
