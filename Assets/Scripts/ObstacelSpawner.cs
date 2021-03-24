959b18892e1cbff4dbef226df6b163d67dce5396
Elisa <elisara1000@hotmail.com>
2021-03-24 22:40:25 +0000

    initial enemy stuff. Doesn't really work. Static enemy that player runs into.

--- Assets/Scripts/ObstacelSpawner.cs
+++ Assets/Scripts/ObstacelSpawner.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class ObstacelSpawner : MonoBehaviour
 {
 
-	//[SerializeField] private GameObject[] obstacles;
-	//[SerializeField] private float spawnInterval = 5.0f;
+	[SerializeField] private GameObject[] obstacles;
+	[SerializeField] private float spawnInterval = 1.0f;
 	private Vector3 spawnPos;
 	private Vector3 playerPos;
 	// Start is called before the first frame update
@@ -14,17 +14,18 @@ public class ObstacelSpawner : MonoBehaviour
     {
 		playerPos = GameObject.Find("Player").GetComponent<Transform>().position;
 		// call method spawn every spawn interval time
-        //InvokeRepeating("Spawn", spawnInterval, spawnInterval);
+        InvokeRepeating("Spawn", spawnInterval, spawnInterval);
     }
 
     // Spawn obstacles
     void Spawn()
     {
-
-		//spawnPos.x = Random.Range(0, 3);
-		//spawnPos.y = 0.5f;
-		//spawnPos.z = Random.Range(0, 3);
-
-		//Instantiate(obstacles[UnityEngine.Random.Range(0, obstacles.Length - 1)], spawnPos, Quaternion.identity);
+		print("Here");
+		print("Player position " + playerPos);
+		spawnPos.x = Random.Range(0, 3) + playerPos.x;
+		spawnPos.y = 0.5f;
+		spawnPos.z = Random.Range(0, 3) + (playerPos.z + 3);
+		print("Spawn postion" + spawnPos);
+		Instantiate(obstacles[UnityEngine.Random.Range(0, obstacles.Length - 1)], spawnPos, Quaternion.identity);
     }
 }
