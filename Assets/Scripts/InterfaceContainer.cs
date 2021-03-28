ec923e8c55f4431f42a00c79e0df9fead76da1b3
cfobrien <conor.obrien24@gmail.com>
2021-03-28 14:53:12 +0100

    refactored code, created NPC type

--- Assets/Scripts/InterfaceContainer.cs
+++ Assets/Scripts/InterfaceContainer.cs
@@ -2,17 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-using Plots;
+using Placeables;
 
 public interface IPlayer {
     void ApplyDamage(int damage);
     void Move(Vector3 keyboardDir);
 }
 
-public interface ITile {
+public interface IPlaceable {
     void MoveTo(Transform transform);
     void MoveTo(Vector3 postion);
     void RotateAround(Vector3 point, Vector3 axis, float angle);
     // void RotateTo(Vector3 axis, float angle);
+    void MoveBy(Vector3 offset);
     void Rotate(float angle);
+}
+
+public interface INPC {
+    void RandomWalk(Vector3 normal, float variance);
+    void Face(Vector3 point);
 }
--- Assets/Scripts/Placeable.cs
+++ Assets/Scripts/Placeable.cs
@@ -1,13 +1,13 @@
 using UnityEngine;
 
-namespace Plots {
-    public class Plot : ITile {
+namespace Placeables {
+    public class Placeable : IPlaceable {
         public GameObject gameObject;
         public Vector3 tileSize;
         public string name;
         public Vector3 rootPos;
 
-        public Plot(GameObject gameObject, Transform transform, string name = null) {
+        public Placeable(GameObject gameObject, Transform transform, string name = null) {
             this.gameObject = gameObject;
             rootPos = this.gameObject.GetComponent<Transform>().position;
             this.gameObject.GetComponent<Transform>().position = transform.position;
@@ -18,7 +18,7 @@ namespace Plots {
             this.name = this.gameObject.name;
             tileSize = this.gameObject.GetComponent<MeshRenderer>().bounds.size;
         }
-        public Plot(GameObject gameObject, Vector3 offset, string name = null) {
+        public Placeable(GameObject gameObject, Vector3 offset, string name = null) {
             this.gameObject = gameObject;
             rootPos = this.gameObject.GetComponent<Transform>().position;
             this.gameObject.GetComponent<Transform>().position += offset;
@@ -28,7 +28,7 @@ namespace Plots {
             this.name = this.gameObject.name;
             tileSize = this.gameObject.GetComponent<MeshRenderer>().bounds.size;
         }
-        public Plot(GameObject gameObject, string name = null) {
+        public Placeable(GameObject gameObject, string name = null) {
             this.gameObject = gameObject;
             rootPos = this.gameObject.GetComponent<Transform>().position;
             if (name != null) {
@@ -46,19 +46,19 @@ namespace Plots {
             gameObject.transform.position = position;
             gameObject.transform.rotation = Quaternion.Euler(0,0,0);
         }
+        public void MoveBy(Vector3 offset) {
+            gameObject.transform.position += offset;
+        }
 
         public void RotateAround(Vector3 point, Vector3 axis, float angle) {
             this.gameObject.GetComponent<Transform>().RotateAround(point, axis, angle);
         }
-        // public void RotateTo(Vector3 axis, float angle) {
-        //     this.gameObject.GetComponent<Transform>().RotateAround(rootPos, axis, angle);
-        // }
         public void Rotate(float angle) {
             this.gameObject.GetComponent<Transform>().RotateAround(rootPos, Vector3.right, angle);
         }
     }
 
-    public class Road : Plot {
+    public class Road : Placeable {
         public Road(GameObject gameObject, Transform transform, string name = null) : base(gameObject, transform, name) {
         }
         public Road(GameObject gameObject, Vector3 position, string name = null) : base(gameObject, position, name) {
@@ -67,12 +67,37 @@ namespace Plots {
         }
     }
 
-    public class Building : Plot {
+    public class Building : Placeable {
         public Building(GameObject gameObject, Transform transform, string name = null) : base(gameObject, transform, name) {
         }
         public Building(GameObject gameObject, Vector3 position, string name = null) : base(gameObject, position, name) {
         }
         public Building(GameObject gameObject, string name = null) : base(gameObject, name) {
+        }
+    }
+
+    public class Obstacle: Placeable {
+        public Obstacle(GameObject gameObject, Transform transform, string name = null) : base(gameObject, transform, name) {
+        }
+        public Obstacle(GameObject gameObject, Vector3 position, string name = null) : base(gameObject, position, name) {
+        }
+        public Obstacle(GameObject gameObject, string name = null) : base(gameObject, name) {
+        }
+    }
+
+    public class NPC: Placeable, INPC {
+        public NPC(GameObject gameObject, Transform transform, string name = null) : base(gameObject, transform, name) {
+        }
+        public NPC(GameObject gameObject, Vector3 position, string name = null) : base(gameObject, position, name) {
+        }
+        public NPC(GameObject gameObject, string name = null) : base(gameObject, name) {
+        }
+        public void RandomWalk(Vector3 normal, float variance) {
+            // TODO: Implement Box-Muller transform
+            return;
+        }
+        public void Face(Vector3 point) {
+            return;
         }
     }
 }
