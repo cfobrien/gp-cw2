14ee75f3d75c2414887dbe5e3f4f22ba2c0aaaaf
Elisa <elisara1000@hotmail.com>
2021-03-28 16:43:25 +0100

    UI for chosing level

---
+++ Assets/Scripts/BtnManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BtnManager : MonoBehaviour
+{
+	public void SendLevel(int lvl){
+		GameManager.Instance.level = lvl;
+		Debug.Log(GameManager.Instance.level);
+		SceneManager.LoadScene(0);
+	}
+}
